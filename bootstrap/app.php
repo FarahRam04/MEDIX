9ae494cb3a9c9e648770212c399ff4e7ae699042
farah <farahrm2004@gmail.com>
2025-06-26 16:05:53 +0300

    Farah

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -23,6 +24,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'is_doctor'=>EnsureUserIsDoctor::class,
             ]);
 
+        //
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
